6ce87c9f810159a50ffb7d1029edea2b8084626f
Teobaldo Lunas <lunasteobaldo@gmail.com>
2025-03-10 21:50:32 -0300

    Se agregan iconos y icono para usuario

--- src/App.jsx
+++ src/App.jsx
@@ -10,51 +10,12 @@ function App() {
 
 
   return (
-    // <div className='bg-rose-300 grid grid-cols-1 sm:grid-cols-4 gap-x-2 gap-y-3 p-2 grid-flow-row-dense'>
-    //   <div className='bg-slate-100 rounded-full shadow-xl w-20 h-20'></div>
-    //   <div className='bg-slate-100 rounded-full shadow-xl w-20 h-20'></div>
-    //   <div className='bg-slate-100 rounded-lg shadow-xl min-h-[50px]'></div>
-    //   <div className='bg-sky-500 rounded-lg shadow-xl min-h-[50px]'></div>
-    //   <div className='bg-slate-100 rounded-lg shadow-xl min-h-[50px]'></div>
-    //   <div className='bg-slate-100 rounded-lg shadow-xl min-h-[50px]'></div>
-    //   <div className='bg-slate-100 rounded-lg shadow-xl min-h-[50px]'></div>
-    //   <div className='bg-slate-100 rounded-lg shadow-xl min-h-[50px]'></div>
-    //   <div className='bg-slate-100 rounded-lg shadow-xl min-h-[50px]'></div>
-    //   <div className='bg-slate-100 rounded-lg shadow-xl min-h-[50px]'></div>
-    //   <div className='bg-slate-100 rounded-lg shadow-xl min-h-[50px]'></div>
-    //   <div className='bg-slate-100 rounded-lg shadow-xl min-h-[50px]'></div>
-    //   <div className='bg-sky-500 rounded-lg shadow-xl min-h-[50px] col-span-2'></div>
-    //   <div className='bg-red-500 rounded-lg shadow-xl min-h-[50px] col-span-2'></div>
-    // </div>
-
     <div className="p-2 grid grid-cols-12 grid-rows-12 gap-3 h-screen">
-      <div className="bg-teal-500 rounded-lg min-h-[50px] col-span-12">
+      <div className="bg-teal-500 rounded-lg col-span-12">
+       <NavBar />
       </div>
       <div className="bg-indigo-500 rounded-lg min-h-[50px] hidden md:block md:col-span-2 md:row-span-11">
-        <div className="bg-teal-500 rounded-lg shadow-xl min-h-[50px] flex items-center m-1 px-3 gap-2">
-        <IoIosAddCircleOutline className='text-2xl'/>
-          <p>opcion</p>
-        </div>
-        <div className="bg-teal-500 rounded-lg shadow-xl min-h-[50px] flex items-center px-3 gap-2 m-1">
-        <IoIosAddCircleOutline className='text-2xl'/>
-          <p>opcion</p>
-        </div>
-        <div className="bg-teal-500 rounded-lg shadow-xl min-h-[50px] flex items-center px-3 gap-2 m-1">
-        <IoIosAddCircleOutline className='text-2xl'/>
-          <p>opcion</p>
-        </div>
-        <div className="bg-teal-500 rounded-lg shadow-xl min-h-[50px] flex items-center px-3 gap-2 m-1">
-        <IoIosAddCircleOutline className='text-2xl'/>
-          <p>opcion</p>
-        </div>
-        <div className="bg-teal-500 rounded-lg shadow-xl min-h-[50px] flex items-center px-3 gap-2 m-1">
-        <IoIosAddCircleOutline className='text-2xl'/>
-          <p>opcion</p>
-        </div>
-        <div className="bg-teal-500 rounded-lg shadow-xl min-h-[50px] flex items-center px-3 gap-2 m-1">
-        <IoIosAddCircleOutline className='text-2xl'/>
-          <p>opcion</p>
-        </div>
+       
       </div>
       <div className="bg-pink-500 rounded-lg min-h-[50px] col-span-12 row-span-11 md:col-span-10 md:row-span-11"></div>
     </div>
--- src/components/navBar/NavBar.jsx
+++ src/components/navBar/NavBar.jsx
@@ -1,12 +1,34 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { IoIosAddCircleOutline } from "react-icons/io";
+import { GrView } from "react-icons/gr";
+import { FaRegUser, FaRegListAlt } from "react-icons/fa";
+
 
 const NavBar = () => {
     return (
-        <nav>
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="/about">About</Link></li>
-            <li><Link to="/products">Products</Link></li>
+        <nav className='flex justify-between items-center px-5 h-16'>
+            {/* Menu opciones */}
+            <ul className='flex gap-2 items-center'>
+                <li>
+                    <IoIosAddCircleOutline className='text-2xl' />
+                </li>
+                <li>
+                    <GrView className='text-2xl' />
+                </li>
+                <li>
+                    <FaRegUser className='text-2xl' />
+                </li>
+                <li>
+                    <FaRegListAlt className='text-2xl' />
+                </li>
+            </ul>
+            {/* Icono usuario */}
+            <div className='flex items-center'>
+                <div className='h-8 w-8 rounded-full bg-red-400 flex items-center justify-center'>
+                    <p className='text-center text-white'>TL</p>
+                </div>
+            </div>
         </nav>
     )
 }
